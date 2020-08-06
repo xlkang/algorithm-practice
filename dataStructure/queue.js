1975ddcb4bbee1cc30d7a7bf99948aa339eeafe4
xinlin kang <xlkang@mail.ustc.edu.cn>
2020-08-06 23:13:47 +0800

    实现队列

--- dataStructure/queue.js
+++ dataStructure/queue.js
@@ -6,31 +6,36 @@ export class Queue {
         this.dataStore = [];
     }
     //入队
-    enqueue () {
-
+    enqueue (el) {
+        this.dataStore.push(el)
     }
     //出队
     dequeue () {
-
+        if(this.empty()) return 'This queue is empty';
+        else return this.dataStore.shift();
     }
     // 查看队首元素
     front () {
-
+        if(this.empty()) return 'This queue is empty';
+        else return this.dataStore[0];
     }
-    //查看队首元素
+    //查看队尾元素
     back () {
-
+        if(this.empty()) return 'This queue is empty';
+        else return this.dataStore[this.dataStore.length - 1];
     }
     // 显示队列所有元素
     toString () {
-
+        return this.dataStore.join('\n');
     }
     //清空当前队列
     clear () {
-
+        delete this.dataStore;
+        this.dataStor = [];
     }
     //判断当前队列是否为空
     empty () {
-
+        if( this.dataStore.length === 0 ) return true;
+        else return false;
     }
 }
