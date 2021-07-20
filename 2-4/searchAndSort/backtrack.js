d4ff9a601d35e2b0c64f16da2d26c63946c4ed63
xinlin kang <xlkang@mail.ustc.edu.cn>
2021-07-20 21:48:56 +0800

    回溯全排列

---
+++ 2-4/searchAndSort/backtrack.js
@@ -0,0 +1,69 @@
+/**
+ * 回溯法
+ * 
+ * 全排列
+ */
+
+/**
+ * 回溯算法核心框架
+ * 1、回溯算法就是一种暴力穷举法。
+ * 2、穷举的过程就是遍历一棵多叉树的过程
+ * 3、回溯算法的代码框架和多叉树遍历的代码框架类似
+ * 
+ * js框架：
+ * let result = []
+ * let 路径 = []
+ * 
+ * function backtrack(选择列表) {
+ *    if(满足结束条件) {
+ *        result.push(路径.join(""))
+ *    }
+ * 
+ *    for(选择： 选择列表) {
+ *        做选择；
+ *        backtrack(选择列表);
+ *        撤销选择
+ *    }
+ * }
+ */
+
+/**
+ * solution
+ * @description 全排列问题
+ * @param {Array}} 选择列表
+ * 
+ * @returns {Array} 可访问格子数
+ */
+function solution(nums) {
+  let result = []
+  // 路径
+  let tracks = []
+
+  function backtrack() {
+    if(tracks.length === nums.length) {
+      // 到达叶子节点
+      result.push(tracks.join(""))
+      return
+    }
+    
+    for(let i = 0; i < nums.length; i++) {
+      if(tracks.includes(nums[i])) continue;
+
+      //记录路径
+      tracks.push(nums[i])
+      //进入下一个节点
+      backtrack(nums)
+      // 退出下一个节点，删除路径
+      tracks.pop()
+    }
+
+    return
+  }
+
+  backtrack()
+
+  return result
+}
+
+const result = solution([1,2,3])
+console.log(result)
--- 2-4/searchAndSort/nQueens.js
+++ 2-4/searchAndSort/nQueens.js
@@ -11,6 +11,29 @@
  * 求坐标
  */
 
+ function check(row, column, size, queens) {
+   console.log("check", queens)
+  // 检查行列
+  for(let k = 0; k < size; k++) {
+    if(queens[k][column] === 'Q') return false
+  }
+      
+  // 检查主对角线  
+  for(let i = 0; i < size; i++) {
+    let j = row + column - i;
+    
+    if(i !== row && queens[i][j] === 'Q') return false
+  }
+
+  // 检查副对角线  
+  for(let i = 0; i < size; i++) {
+    let j = column - row + i;
+    if(i !== row && queens[i][j] === 'Q') return false
+  }
+
+  return true
+}
+
 /**
  * getNQueenCount
  * @description N皇后问题
@@ -20,61 +43,55 @@
  */
 function getNQueenCount(size = 4) {
   let queens = []
+  let result = []
   let count = 0
-  let a = 0;
   let i = 0
   while(i < size) {
     queens.push([])
     i++
   }
 
-  function check(row, column, size) {
-    // 检查行列
-    for(let k = 0; k < size; k++) {
-      if(queens[k][column] === 1) return false
-    }
-        
-    // 检查主对角线  
-    for(let i = 0; i < size; i++) {
-      let j = row + column - i;
-      if(i !== row && queens[i][j] === 1) return false
-    }
-
-    // 检查主对角线  
-    for(let i = 0; i < size; i++) {
-      let j = column - row+ i;
-      if(i !== row && queens[i][j] === 1) return false
-    }
-
-    return true
-  }
-
-  function findQueen(row) {
-    a += 1
+  function findQueen(row, queens) {
     if(row > size - 1) {
       // 8行选择结束，记录一个count
       count += 1
+      console.log(queens)
+      result.push(queens)
       return;
     }
     
+    // 列循环
     for(let j = 0; j < size; j++) {
-      if(!queens[row][j] && check(row, j, size)) {
+      if(!queens[row][j] && check(row, j, size, queens)) {
         // 做选择
-        queens[row][j] = 1
+        queens[row][j] = 'Q'
 
-        findQueen(row+1)
+        // let path = []
+        // for(let k = 0; k < size; k++) {
+        //   if(k === j) {
+        //     path.push("Q")
+        //   } else {
+        //     path.push(".")
+        //   }
+        // }
+
+        // result[row] = path.join("")
+        
+        findQueen(row + 1, queens)
+        queens[row][j] = '.'
       }
-      
       // 取消选择
-      queens[row][j] = 0
+      // temp[row][j] = 0
     }
   }
 
-  findQueen(0)
+  findQueen(0, queens)
 
-  console.log("执行次数", a)
-  return count
+  return {
+    count,
+    queens: result
+  }
 }
 
-const result = getNQueenCount(8)
-console.log(result)
+// const result = getNQueenCount(4)
+// console.log(result)
